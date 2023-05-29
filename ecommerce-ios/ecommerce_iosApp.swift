0b9ef15310f06668d5d37eb0d898b00907fff518
= <kelvin.prayitno@ctcorpdigital.com>
2023-05-29 16:57:24 +0700

    Initial Commit

---
+++ ecommerce-ios/ecommerce_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecommerce_iosApp.swift
+//  ecommerce-ios
+//
+//  Created by Kelvin Prayitno on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ecommerce_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
